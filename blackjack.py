270363c7457d2486f674fbe591b7e766841653d0
anthonyreddygopu <anthonyreddygopu@gmail.com>
2022-01-06 18:48:45 +0530

    Modified blackjack.py and add goputest.py files

--- blackjack.py
+++ blackjack.py
@@ -121,6 +121,9 @@ def new_game():
 def shuffle():
     random.shuffle(deck)
 
+def gopu():
+    print("Anthony Reddy Gopu")
+
 mainWindow = tkinter.Tk()
 
 # Set up the screen and frames for the dealer and player
